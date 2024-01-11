7e01b3e9dd65f661518f10a86f901ba34d91916f
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-11 16:09:19 +0000

    Lear concept of component did update inside parent

--- my-app/src/components/LifeCyclesCDU.js
+++ my-app/src/components/LifeCyclesCDU.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
+import LifeCyclesCDUChild from './LifeCyclesCDUChild'
 
 export class LifeCyclesCDU extends Component {
     constructor(props) {
-        
+        console.log('Parent constructor')
         super(props)
         this.state = {
             greeting: 'Hello!'
         }
     }
-    
+    updateGreetings = () => {
+        console.log('Parent update greeting called')
+        this.setState(prevState => {
+            return{
+                greeting: prevState.greeting === 'Hello!' ? 'Goodbye' : 'Hello!',
+            }
+        })
+    }
+    componentDidUpdate() {
+        console.log('Parent component did update')
+    }
     render() {
+        console.log('Parent render method')
         return (
             <div>
                 <h1>{this.state.greeting}</h1>
+                <button onClick={this.updateGreetings}>Update Greetings</button>
+                <LifeCyclesCDUChild />
             </div>
         )
     }
